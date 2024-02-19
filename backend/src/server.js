051324eb52d375602a29ded0659a78b9a0348bda
Ana Paula Assunção <anap.assuncao03@gmail.com>
2024-02-19 18:31:05 -0300

    :tada: new repo

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import { WebSocketServer } from "ws"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const wss = new WebSocketServer({ port: process.env.PORT || 3000 })
+
+wss.on("connection", (ws) => {
+    ws.on("error", console.error)
+
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log("Client connected")
+})
